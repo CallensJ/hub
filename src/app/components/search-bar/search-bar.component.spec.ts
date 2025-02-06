56f95414971074fc83c2614cb510d566ad1147b2
CallensJ <callens.johan@protonmail.com>
2025-02-06 13:12:15 +0100

    add searchbarcomponent

--- src/app/components/search-bar/search-bar.component.spec.ts
+++ src/app/components/search-bar/search-bar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ComposantTestComponent } from './composant-test.component';
+import { SearchBarComponent } from './search-bar.component';
 
-describe('ComposantTestComponent', () => {
-  let component: ComposantTestComponent;
-  let fixture: ComponentFixture<ComposantTestComponent>;
+describe('SearchBarComponent', () => {
+  let component: SearchBarComponent;
+  let fixture: ComponentFixture<SearchBarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ComposantTestComponent]
+      imports: [SearchBarComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ComposantTestComponent);
+    fixture = TestBed.createComponent(SearchBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
